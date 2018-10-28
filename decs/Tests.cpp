a9bf11176103839493c1c7652778ae7c10ce3e14
Victor Blanco <vblanco20-1@hotmail.com>
2018-10-28 22:48:35 +0100

    Added more tests

--- decs/Tests.cpp
+++ decs/Tests.cpp
@@ -66,6 +66,23 @@ TEST_CASE("Archetype: 2 Components Added - 2 Removed") {
 	REQUIRE(PosRot.componentlist.metatypes.size() == 0);
 }
 
+TEST_CASE("Archetype: 2 Components Added - Repeated remove") {
+
+	//archetype with posrot
+	Archetype PosRot;
+	PosRot.AddComponent<Position>();
+	PosRot.AddComponent<Rotation>();
+
+	PosRot.RemoveComponent<Rotation>();
+	PosRot.RemoveComponent<Rotation>();
+	PosRot.RemoveComponent<Rotation>();
+	PosRot.RemoveComponent<Rotation>();
+	PosRot.RemoveComponent<Rotation>();
+
+	//check size is 2
+	REQUIRE(PosRot.componentlist.metatypes.size() == 1);
+}
+
 TEST_CASE("Entity Creation: 1 Entity") {
 	//archetype with posrot
 	Archetype PosRot;
@@ -82,7 +99,7 @@ TEST_CASE("Entity Creation: 1 Entity") {
 	PosRotBig.AddComponent<BigComponent>();
 
 	ECSWorld ECS;
-	ECS.Blocks.reserve(20);
+	
 
 	SECTION("Adding Pos + Rot (2 Components)") {
 
@@ -150,7 +167,7 @@ TEST_CASE("Entity Creation: 1 Entity - Batched") {
 	PosRotBig.AddComponent<BigComponent>();
 
 	ECSWorld ECS;
-	ECS.Blocks.reserve(20);
+	
 
 	SECTION("Adding Pos + Rot (2 Components)") {
 
@@ -197,5 +214,215 @@ TEST_CASE("Entity Creation: 1 Entity - Batched") {
 		REQUIRE(ECS.BlockStorage[0].first->entities[0] == handle);
 		//entity stores the correct pointer
 		REQUIRE(ECS.Entities[0].block == ECS.BlockStorage[0].first);
+	}
+}
+
+
+TEST_CASE("Entity Creation: 1 Maxed Entity Block - Batched") {
+	//archetype with posrot
+	Archetype PosRot;
+	PosRot.AddComponent<Position>();
+	PosRot.AddComponent<Rotation>();
+
+	Archetype PosOnly;
+	PosOnly.AddComponent<Position>();
+
+
+	Archetype PosRotBig;
+	PosRotBig.AddComponent<Position>();
+	PosRotBig.AddComponent<Rotation>();
+	PosRotBig.AddComponent<BigComponent>();
+
+	ECSWorld ECS;
+	
+
+	SECTION("Adding Pos + Rot (2 Components)") {
+
+		EntityHandle handle = ECS.CreateEntityBatched(PosRot, ARRAY_SIZE)[0];
+
+		REQUIRE(handle.id == 0);
+		REQUIRE(handle.generation == 1);
+		REQUIRE(ECS.BlockStorage.size() == 1);
+		REQUIRE(ECS.BlockStorage[0].nblocks == 1);
+		//test block validity
+		REQUIRE(ECS.BlockStorage[0].first != nullptr);
+		REQUIRE(ECS.BlockStorage[0].last != nullptr);		
+		//block stores the correct handle
+		REQUIRE(ECS.BlockStorage[0].first->entities[0] == handle);
+		//entity stores the correct pointer
+		REQUIRE(ECS.Entities[0].block == ECS.BlockStorage[0].first);
+	}
+	SECTION("Adding Pos + Rot + Big(3 Components)") {
+		EntityHandle handle = ECS.CreateEntityBatched(PosRot, ARRAY_SIZE)[0];
+
+		REQUIRE(handle.id == 0);
+		REQUIRE(handle.generation == 1);
+		REQUIRE(ECS.BlockStorage.size() == 1);
+		REQUIRE(ECS.BlockStorage[0].nblocks == 1);
+		//test block validity
+		REQUIRE(ECS.BlockStorage[0].first != nullptr);
+		REQUIRE(ECS.BlockStorage[0].last != nullptr);
+		
+		//block stores the correct handle
+		REQUIRE(ECS.BlockStorage[0].first->entities[0] == handle);
+		//entity stores the correct pointer
+		REQUIRE(ECS.Entities[0].block == ECS.BlockStorage[0].first);
+	}
+	SECTION("Adding Pos (1 Components)") {
+		EntityHandle handle = ECS.CreateEntityBatched(PosRot, ARRAY_SIZE)[0];
+
+		REQUIRE(handle.id == 0);
+		REQUIRE(handle.generation == 1);
+		REQUIRE(ECS.BlockStorage.size() == 1);
+		REQUIRE(ECS.BlockStorage[0].nblocks == 1);
+		//test block validity
+		REQUIRE(ECS.BlockStorage[0].first != nullptr);
+		REQUIRE(ECS.BlockStorage[0].last != nullptr);
+		
+		//block stores the correct handle
+		REQUIRE(ECS.BlockStorage[0].first->entities[0] == handle);
+		//entity stores the correct pointer
+		REQUIRE(ECS.Entities[0].block == ECS.BlockStorage[0].first);
+	}
+}
+
+
+TEST_CASE("Entity Creation: 1 Maxed Entity Block + 1 - Batched") {
+	//archetype with posrot
+	Archetype PosRot;
+	PosRot.AddComponent<Position>();
+	PosRot.AddComponent<Rotation>();
+
+	Archetype PosOnly;
+	PosOnly.AddComponent<Position>();
+
+
+	Archetype PosRotBig;
+	PosRotBig.AddComponent<Position>();
+	PosRotBig.AddComponent<Rotation>();
+	PosRotBig.AddComponent<BigComponent>();
+
+	ECSWorld ECS;
+	
+
+	SECTION("Adding Pos + Rot (2 Components)") {
+
+		EntityHandle handle = ECS.CreateEntityBatched(PosRot, ARRAY_SIZE+1)[0];
+
+		REQUIRE(handle.id == 0);
+		REQUIRE(handle.generation == 1);
+		REQUIRE(ECS.BlockStorage.size() == 1);
+		REQUIRE(ECS.BlockStorage[0].nblocks == 2);
+		//test block validity
+		REQUIRE(ECS.BlockStorage[0].first != nullptr);
+		REQUIRE(ECS.BlockStorage[0].last != nullptr);
+		//block stores the correct handle
+		REQUIRE(ECS.BlockStorage[0].first->entities[0] == handle);
+		//entity stores the correct pointer
+		REQUIRE(ECS.Entities[0].block == ECS.BlockStorage[0].first);
+	}
+	SECTION("Adding Pos + Rot + Big(3 Components)") {
+		EntityHandle handle = ECS.CreateEntityBatched(PosRot, ARRAY_SIZE+1)[0];
+
+		REQUIRE(handle.id == 0);
+		REQUIRE(handle.generation == 1);
+		REQUIRE(ECS.BlockStorage.size() == 1);
+		REQUIRE(ECS.BlockStorage[0].nblocks == 2);
+		//test block validity
+		REQUIRE(ECS.BlockStorage[0].first != nullptr);
+		REQUIRE(ECS.BlockStorage[0].last != nullptr);
+
+		//block stores the correct handle
+		REQUIRE(ECS.BlockStorage[0].first->entities[0] == handle);
+		//entity stores the correct pointer
+		REQUIRE(ECS.Entities[0].block == ECS.BlockStorage[0].first);
+	}
+	SECTION("Adding Pos (1 Components)") {
+		EntityHandle handle = ECS.CreateEntityBatched(PosRot, ARRAY_SIZE+1)[0];
+
+		REQUIRE(handle.id == 0);
+		REQUIRE(handle.generation == 1);
+		REQUIRE(ECS.BlockStorage.size() == 1);
+		REQUIRE(ECS.BlockStorage[0].nblocks == 2);
+		//test block validity
+		REQUIRE(ECS.BlockStorage[0].first != nullptr);
+		REQUIRE(ECS.BlockStorage[0].last != nullptr);
+
+		//block stores the correct handle
+		REQUIRE(ECS.BlockStorage[0].first->entities[0] == handle);
+		//entity stores the correct pointer
+		REQUIRE(ECS.Entities[0].block == ECS.BlockStorage[0].first);
+	}
+}
+
+
+TEST_CASE("Entity Creation: 1 Million entities - Batched") {
+	//archetype with posrot
+	Archetype PosRot;
+	PosRot.AddComponent<Position>();
+	PosRot.AddComponent<Rotation>();
+
+	Archetype PosOnly;
+	PosOnly.AddComponent<Position>();
+
+
+	Archetype PosRotBig;
+	PosRotBig.AddComponent<Position>();
+	PosRotBig.AddComponent<Rotation>();
+	PosRotBig.AddComponent<BigComponent>();
+
+	ECSWorld ECS;
+	
+
+	SECTION("Adding Pos + Rot (2 Components)") {
+
+		EntityHandle handle = ECS.CreateEntityBatched(PosRot,  1000000)[0];
+
+		REQUIRE(handle.id == 0);
+		REQUIRE(handle.generation == 1);
+		REQUIRE(ECS.BlockStorage.size() == 1);
+		//REQUIRE(ECS.BlockStorage[0].nblocks == 2);
+		//test block validity
+		REQUIRE(ECS.BlockStorage[0].first != nullptr);
+		REQUIRE(ECS.BlockStorage[0].last != nullptr);
+		//block stores the correct handle
+		REQUIRE(ECS.BlockStorage[0].first->entities[0] == handle);
+		//entity stores the correct pointer
+		REQUIRE(ECS.Entities[0].block == ECS.BlockStorage[0].first);
+		REQUIRE(ECS.ValidateAll());
+	}
+	SECTION("Adding Pos + Rot + Big(3 Components)") {
+		EntityHandle handle = ECS.CreateEntityBatched(PosRot, 1000000)[0];
+
+		REQUIRE(handle.id == 0);
+		REQUIRE(handle.generation == 1);
+		REQUIRE(ECS.BlockStorage.size() == 1);
+		//REQUIRE(ECS.BlockStorage[0].nblocks == 2);
+		//test block validity
+		REQUIRE(ECS.BlockStorage[0].first != nullptr);
+		REQUIRE(ECS.BlockStorage[0].last != nullptr);
+
+		//block stores the correct handle
+		REQUIRE(ECS.BlockStorage[0].first->entities[0] == handle);
+		//entity stores the correct pointer
+		REQUIRE(ECS.Entities[0].block == ECS.BlockStorage[0].first);
+		REQUIRE(ECS.ValidateAll());
+	}
+	SECTION("Adding Pos (1 Components)") {
+		EntityHandle handle = ECS.CreateEntityBatched(PosRot, 1000000)[0];
+
+		REQUIRE(handle.id == 0);
+		REQUIRE(handle.generation == 1);
+		REQUIRE(ECS.BlockStorage.size() == 1);
+		//REQUIRE(ECS.BlockStorage[0].nblocks == 2);
+		//test block validity
+		REQUIRE(ECS.BlockStorage[0].first != nullptr);
+		REQUIRE(ECS.BlockStorage[0].last != nullptr);
+
+		//block stores the correct handle
+		REQUIRE(ECS.BlockStorage[0].first->entities[0] == handle);
+		//entity stores the correct pointer
+		REQUIRE(ECS.Entities[0].block == ECS.BlockStorage[0].first);
+		REQUIRE(ECS.ValidateAll());
 	}
 }
